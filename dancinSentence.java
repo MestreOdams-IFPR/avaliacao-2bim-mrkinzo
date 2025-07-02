f0361b846b2d8994a646a2ce4ef0af1e4bdba3be
mrKinzo <eduardokinzo@gmail.com>
2025-07-02 09:56:00 -0300

    saida indesejada

--- dancinSentence.java
+++ dancinSentence.java
@@ -7,36 +7,58 @@ public class dancinSentence {
     public static void main(String[] args) {
         String Sentence = LER.nextLine();
         String veString[] = Sentence.split(" ");
-        veString=transformTheSentence(veString);
-        Sentence=unirVtString(veString);
+
+        veString = transformTheSentence(veString);
+
+        Sentence = unirVtString(veString);
         System.out.println(Sentence);
-            }
-        
-            public  static String unirVtString(String[] veString) {
-                String Assembly=String.join(" ", veString);
-                return Assembly;
-            }
-        
-            public static String[] transformTheSentence(String[] veString) {
-        for (int i = 0; i < veString.length; i++) {
+    }
+
+    public static String unirVtString(String[] veString) {
+        String Assembly = String.join(" ", veString);
+        return Assembly;
+    }
+
+    public static String[] transformTheSentence(String[] veString) {
+        String x = "";
+        for (int i = 0; i < veString.length - 1; i++) {
             char[] vetCh = veString[i].toCharArray();
             for (int j = 0; j < vetCh.length; j++) {
-                boolean firtCharUCase=checkIfUpper(vetCh);
-                if (firtCharUCase!=true) {
-                    vetCh[j++]=Character.toUpperCase(vetCh[j++]);
+                if (checkIfUpper(vetCh) == false) {
+                    vetCh[j++] = Character.toUpperCase(vetCh[j]);
+                } else {
+                    checkIfLower(vetCh);
+                    if (checkIfLower(vetCh) == true) {
+                        vetCh[j] = Character.toLowerCase(vetCh[j]);
+                    }
                 }
+
+                x += vetCh;
             }
-            veString[i]=String.copyValueOf(vetCh);
+            veString[i] = x;
         }
         return veString;
     }
 
-    public  static boolean  checkIfUpper(char[] vetCh) {
-        boolean Uc=false;
-     Character ch = vetCh[0];
-        if (ch.isUpperCase(vetCh[0])) {
-            return Uc=true;
+    public static boolean checkIfUpper(char[] vetCh) {
+        boolean Uc = false;
+        for (int i = 0; i < vetCh.length; i++) {
+
+            if (Character.isUpperCase(vetCh[i])) {
+                return Uc = true;
+            }
         }
-        return  Uc;
+        return Uc;
+    }
+
+    public static boolean checkIfLower(char[] vetCh) {
+        boolean Lc = false;
+
+        for (int i = 0; i < vetCh.length; i++) {
+            if (Character.isLowerCase(vetCh[i])) {
+                return Lc = true;
+            }
+        }
+        return Lc;
     }
 }
